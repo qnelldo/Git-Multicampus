424e15b39d86ca8a82a1795aa6916888010d1df7
qnelldo <131656049+qnelldo@users.noreply.github.com>
2023-05-30 14:16:46 +0900

    mvn static-code-analysis

---
+++ src/main/java/com/main/SQLInjectionExample.java
@@ -0,0 +1,18 @@
+package com.main;
+
+public class SQLInjectionExample {
+    public static void main(String[] args) {
+        String username = getUserInput();
+        String query = "SELECT * FROM users WHERE username = '" + username + "'";
+        // query 실행 로직
+        System.out.println(query);
+    }
+//쿼리 자체를 변수로 설정하는 게 문제임.
+
+    private static String getUserInput() {
+        // 사용자로부터 입력 받는 로직
+        return "admin' --";
+    }
+}
+
+
