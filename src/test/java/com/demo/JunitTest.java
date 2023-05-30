275761ad3df3601ada43ec2998a70aa41e3ecc23
qnelldo <131656049+qnelldo@users.noreply.github.com>
2023-05-30 11:38:25 +0900

    mvn test

---
+++ src/test/java/com/demo/JunitTest.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import org.junit.Test;
+
+public class JunitTest {
+    @Test
+    public void getMulticampus () {
+
+        //하나의 기능만 동작하는 게 아니라
+        //여러 개의 기능이 들어가는 경우가 있음
+        //10개 기능을 하나에 욱여넣더라도 테스트 코드로 10개 쪼개서 테스트하게끔 하세요!
+
+        System.out.println("Multicampus");
+    }
+}
